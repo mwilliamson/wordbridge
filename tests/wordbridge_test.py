98b52649fc57b6034029398a002701061872ea2f
Michael Williamson <mike@zwobble.org>
2012-11-03 20:02:48 +0000

    Test using fake docx file rather than document XML string

--- tests/wordbridge_test.py
+++ tests/wordbridge_test.py
@@ -4,8 +4,18 @@ import wordbridge
 
 @istest
 def word_document_containing_one_pararaph_is_converted_to_single_p_element():
-    result = wordbridge.convert_string_to_html(_SIMPLE_WORD_DOCUMENT)
+    docx_file = FakeDocxFile({
+        "word/document.xml": _SIMPLE_WORD_DOCUMENT
+    })
+    result = wordbridge.convert_to_html(docx_file)
     assert_equal("<p>Hello.</p>", result)
+
+class FakeDocxFile(object):
+    def __init__(self, files):
+        self._files = files
+      
+    def read(self, name):
+        return self._files[name]
 
 _SIMPLE_WORD_DOCUMENT = """<?xml version="1.0" ?>
 <w:document mc:Ignorable="w14 wp14" xmlns:m="http://schemas.openxmlformats.org/officeDocument/2006/math" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main" xmlns:w10="urn:schemas-microsoft-com:office:word" xmlns:w14="http://schemas.microsoft.com/office/word/2010/wordml" xmlns:wne="http://schemas.microsoft.com/office/word/2006/wordml" xmlns:wp="http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing" xmlns:wp14="http://schemas.microsoft.com/office/word/2010/wordprocessingDrawing" xmlns:wpc="http://schemas.microsoft.com/office/word/2010/wordprocessingCanvas" xmlns:wpg="http://schemas.microsoft.com/office/word/2010/wordprocessingGroup" xmlns:wpi="http://schemas.microsoft.com/office/word/2010/wordprocessingInk" xmlns:wps="http://schemas.microsoft.com/office/word/2010/wordprocessingShape">
--- wordbridge/__init__.py
+++ wordbridge/__init__.py
@@ -7,7 +7,9 @@ import wordbridge.openxml
 from wordbridge.htmlgeneration import HtmlGenerator
 from wordbridge import styles
 
-def convert_string_to_html(document_string):
+def convert_to_html(docx_file):
+    document_string = docx_file.read("word/document.xml")
+    print document_string
     tree = etree.fromstring(document_string)
     document = wordbridge.openxml.read_document(tree)
     
@@ -16,8 +18,7 @@ def convert_string_to_html(document_string):
 
 def convert_docx_file_to_html(path):
     with zipfile.ZipFile(path, "r") as zip_file:
-        string = zip_file.read("word/document.xml")
-        return convert_string_to_html(string)
+        return convert_to_html(zip_file)
 
 def _create_paragraph_styles():
     return {
